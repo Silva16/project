2516b428efd53449f83093d231143db1b8387297
jebazz <sousa.scp@gmail.com>
2015-06-01 14:51:57 +0100

    teste 1

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,20 +1,9 @@
-<html>
-	<head>
-		<title>Laravel</title>
-		
-		<link href='//fonts.googleapis.com/css?family=Lato:100' rel='stylesheet' type='text/css'>
+@extends('header')
+@section('content')
+
+
 
 		<style>
-			body {
-				margin: 0;
-				padding: 0;
-				width: 100%;
-				height: 100%;
-				color: #B0BEC5;
-				display: table;
-				font-weight: 100;
-				font-family: 'Lato';
-			}
 
 			.container {
 				text-align: center;
@@ -27,24 +16,17 @@
 				display: inline-block;
 			}
 
-			.title {
+			.content.title {
 				font-size: 96px;
 				margin-bottom: 40px;
 			}
-
-			.quote {
-				font-size: 24px;
-			}
 		</style>
-	</head>
-	<body>
-		<div class="container">
-			<div class="content">
-				<div class="title">Laravel 5 - Test</div>
-				<div class="container">
 
-				</div>
-			</div>
+
+
+			<div class="content">
+
 		</div>
-	</body>
-</html>
+
+@endsection
+@extends('footer')
