a0e9fc70928fcdf679dd1dbdef66c3dee569b428
jebazz <sousa.scp@gmail.com>
2015-06-02 02:18:00 +0100

    teste 1

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,25 +1,31 @@
  <footer>
-    <div class="container">
-        <div class="col-md-4">
+        <div class="content">
+        <div class="col-md-4 ">
 
                 <img id="footerlogo" src="css/imagens/logoipl.png" alt="logoipl" />
         </div>
-        <div class="col-md-4">
-                <ul><li>Rua General Norton de Matos,</li>
-                <li> Apartado 4133,</li>
-                <li>2411-901 Leiria – Portugal</li>
+        <div class="col-md-4 vcenter ">
+                <ul><li class="footercenter">Rua General Norton de Matos,</li>
+                <li class="footercenter"> Apartado 4133,</li>
+                <li class="footercenter">2411-901 Leiria – Portugal</li>
                     </ul>
         </div>
-        <div class="col-md-4">
-                <p>
-                    <span class="bold"> GPS: </span>39°44’15.1″N 8°48’40.8″W
-                    <br /><span class="bold">Telefone: </span> (+351) 244830010
-                    <br /><span class="bold">E-mail: </span>ipleiria@ipleiria.pt
-                </p>
+        <div class="col-md-4 align">
+                <ul>
+                    <li class="footerinfo" ><span class="bold">GPS: </span>39°44’15.1″N 8°48’40.8″W</li>
+                    <li class="footerinfo"><span class="bold">Telefone: </span> (+351) 244830010</li>
+                    <li class="footerinfo"><span class="bold">E-mail: </span>ipleiria@ipleiria.pt</li>
+               </ul>
+        </div>
+        </div>
+
+        <div class="endfooter">
+            <a  href="http://www.ipleiria.pt/" >©2014-2015 Politécnico de Leiria</a>
+
         </div>
 
 
-    </div>
+
 </footer>
 </body>
 </html>
